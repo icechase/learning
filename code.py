3712156b06f059b36ba60bdffd4aa8f7d9e047a7
chase <icechase@gmail.com>
2015-10-02 17:21:21 +0800

    code.py

--- code.py
+++ code.py
@@ -3,7 +3,17 @@
 print "hello world "
 
 print " you are beautiful girls."
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print " you are a beautiful boy."
+=======
+print " you are a beautiful man."
+print " you are a bad man."
+>>>>>>> Stashed changes
+=======
+print " you are a beautiful man."
+print " you are a bad man."
+>>>>>>> Stashed changes
 
 print "I am third "
 print "I am third "
